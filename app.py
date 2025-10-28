6a92269ef1d56d93e1097db66d471473361d84e8
meghanaraparthi <meghanaraparthi657@gmail.com>
2025-10-28 22:12:57 +0530

    firstcommit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+    return render_template('myform.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    uname = request.form['uname']
+    return render_template('greeting.html', name=uname)
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000, debug=True)
